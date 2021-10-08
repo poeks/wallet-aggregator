5c68d549fe32474c3350c697015a70c44b9ac2a5
Roel <roelwinter11@gmail.com>
2021-10-08 11:37:22 +0200

    Add Kucoin integration

--- api/config.py
+++ api/config.py
@@ -1,4 +1,16 @@
+from typing import Optional
+
 from pydantic import BaseSettings
+
+
+class IncompleteSettingsError(ValueError):
+    pass
+
+
+class KuCoinCredentials(BaseSettings):
+    kucoin_api_key: str
+    kucoin_secret: str
+    kucoin_passphrase: str
 
 
 class Settings(BaseSettings):
@@ -7,5 +19,19 @@ class Settings(BaseSettings):
     binance_secret: str
     coinmarketcap_api_key: str
 
+    kucoin_api_key: Optional[str] = None
+    kucoin_secret: Optional[str] = None
+    kucoin_passphrase: Optional[str] = None
+
     class Config:
         env_file = ".env"
+
+    def get_kucoin_credentials(self) -> KuCoinCredentials:
+        if not all({self.kucoin_api_key, self.kucoin_secret, self.kucoin_passphrase}):
+            raise IncompleteSettingsError("Kucoin credentials incomplete")
+
+        return KuCoinCredentials(
+            kucoin_api_key=self.kucoin_api_key,
+            kucoin_secret=self.kucoin_secret,
+            kucoin_passphrase=self.kucoin_passphrase,
+        )
---
+++ api/services/kucoin.py
@@ -0,0 +1,89 @@
+import base64
+import hashlib
+import hmac
+import time
+from collections import defaultdict
+from datetime import datetime
+from typing import ClassVar
+from typing import Dict
+from typing import List
+
+import requests as r
+from pydantic import BaseModel
+
+from config import Settings
+from schemas import Balance
+from schemas import Wallet
+
+KUCOIN_HOST = "https://api.kucoin.com"
+
+
+class _KucoinAsset(BaseModel):
+    currency: str
+    available: float
+    balance: float
+    holds: float
+    id: str
+    type: str
+
+
+class KuCoinGETAccountResponse(BaseModel):
+    data: List[_KucoinAsset]
+
+
+def get_kucoin_wallet() -> "Wallet":
+    """
+    See https://docs.kucoin.com/ for reference.
+
+    All timestamps are in miliseconds.
+    """
+    # settings = Settings()
+    # s.verify_kucoin_credentials()
+    s = Settings().get_kucoin_credentials()
+
+    # See https://docs.kucoin.com/#authentication.
+    now_time = int(time.time()) * 1000
+    method = "GET"
+    uri_path = "/api/v1/accounts"
+    url = KUCOIN_HOST + uri_path
+    str_to_sign = str(now_time) + method + uri_path
+    sign = base64.b64encode(
+        hmac.new(
+            s.kucoin_secret.encode("utf-8"), str_to_sign.encode("utf-8"), hashlib.sha256
+        ).digest()
+    )
+    passphrase = base64.b64encode(
+        hmac.new(
+            s.kucoin_secret.encode("utf-8"),
+            s.kucoin_passphrase.encode("utf-8"),
+            hashlib.sha256,
+        ).digest()
+    )
+
+    headers = {
+        "KC-API-SIGN": sign,
+        "KC-API-TIMESTAMP": str(now_time),
+        "KC-API-KEY": s.kucoin_api_key,
+        "KC-API-PASSPHRASE": passphrase,
+        "Content-Type": "application/json",
+        "KC-API-KEY-VERSION": "2",
+    }
+
+    res = r.get(url, headers=headers)
+
+    if not res.ok:
+        raise ValueError(f"Kucoin service unavailable")
+
+    data = res.json()
+
+    # Parse balances.
+    # Kucoin has 3 account types: main, trade, and margin.
+    balances: Dict[str, float] = defaultdict(int)
+    for balance in data["data"]:
+        balances[balance["currency"]] += float(balance["balance"])
+
+    aggregated_balances = [
+        Balance(symbol=symbol, amount=amount) for symbol, amount in balances.items()
+    ]
+
+    return Wallet(name="Kucoin", date=datetime.now(), balances=aggregated_balances)
